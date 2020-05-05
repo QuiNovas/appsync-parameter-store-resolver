8d0cb42115d4ba77820b27437850c66d4dad7785
Joseph Wortmann <joseph.wortmann@gmail.com>
2020-05-05 11:35:56 -0500

    fix to actually return a list when nextToken

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ lambda_name = 'appsync-parameter-store-resolver'
 # Versions should comply with PEP440.  For a discussion on single-sourcing
 # the version across setup.py and the project code, see
 # https://packaging.python.org/en/latest/single_source_version.html
-lambda_version = '0.0.1'
+lambda_version = '0.0.2'
 
 lambda_description = 'AWS Lambda designed to resolve appsync requests to Parameter Store.'
 
